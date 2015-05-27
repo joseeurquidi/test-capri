309b14e689a6737425c93d653e674f6eef0fd1d4
joseeurquidi <josee_urquidi@outlook.com>
2015-05-27 15:42:17 -0500

    capri

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,21 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-
+# production deployment
+set :stage, :production
+# use the master branch of the repository
+set :branch, "master"
+# the user login on the remote server
+# used to connect and deploy
+set :deploy_user, "urquidi"
+# the 'full name' of the application
+set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
+# the server(s) to deploy to
+server 'test-capri.cloudapp.net/', user: 'urquidi', roles: %w{web app db}, primary: true
+# the path to deploy to
+set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
+# set to production for Rails
+set :rails_env, :production
 
 # role-based syntax
 # ==================
